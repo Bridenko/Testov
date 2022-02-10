8aad1736a7224bb562eacfce04ab36b231c8d9bc
bridenko <glomokomo@gmail.com>
2022-02-11 00:10:06 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@
     const burgerItem = document.querySelector('.header-mob__burger'); /* При нажатии на бургер */
     const menu = document.querySelector('.burger-menu'); /* Ищем меню */
     const menuCloseItem = document.querySelector('.burger-menu__close'); /* Нажимая на кнопу close закрываем выпадающее меню */
+    const body = document.querySelector('body');
     // const menuLinks = document.querySelectorAll('.burger-menu__link'); /* При нажатии на ссылки закрывается выпадающее меню */
     burgerItem.addEventListener('click', () => {         /* Активируем выпадающее меню */
-        menu.classList.add('is-active-menu')
+        menu.classList.add('is-active-menu');
     });
     menuCloseItem.addEventListener('click', () => {      /* Нажимая на кнопу close закрываем выпадающее меню */
-        menu.classList.remove('is-active-menu')
+        menu.classList.remove('is-active-menu');
     });
     if (window.innerWidth <= 767) {
         for (let i = 0; i < menuLinks.length; i += 1) {    /* При нажатии на ссылки закрывается выпадающее меню */
